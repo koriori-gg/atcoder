106800778b44683e5da314dd55fa1b84fa524b32
koriori <maaacha.kuri05@gmail.com>
2023-06-15 17:27:27 +0900

    add: 拡張子をpythonに変えるファイルを追加

---
+++ rename_extension_to_python.py
@@ -0,0 +1,20 @@
+import os
+
+path = "./submissions"
+
+def rename_file_extension(path):
+	directorys = os.listdir(path)
+	for directory in directorys:
+		directory_path = path + '/' + directory
+		files = os.listdir(directory_path)
+		for file in files:
+			print(directory_path + '/' + file)
+			if os.path.isdir(directory_path + '/' + file):
+				continue
+			name, extension = os.path.splitext(file)
+			if extension != '.py':
+				new_file = name + ".py"
+				os.rename(directory_path + '/' + name + extension, directory_path + '/' + new_file)
+
+path = "./submissions"
+rename_file_extension(path)
